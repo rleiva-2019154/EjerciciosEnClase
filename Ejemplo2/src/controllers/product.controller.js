9b538ac386f39693fe16c83262896cdc6d4fa19c
rleiva-2019154 <rleiva-2019154@kinal.edu.gt>
2024-02-02 10:23:57 -0600

    Se hizo realiza conexion a db

--- Ejemplo2/src/controllers/product.controller.js
+++ Ejemplo2/src/controllers/product.controller.js
@@ -2,13 +2,25 @@
 
 const Product = require('../models/product.model') //<- Objeto / Instancia
 
+//version obsoleta
                     //request / response
 exports.newProduct = (req, res)=>{
     let data = req.body; //capturar la informacion que me envia el usuario
-    if(Object.keys(data).length == 0) res.status(400).send({mesage: 'Missin data'})
-    let product = new Product(data)
+    if(Object.keys(data).length == 0) return res.status(400).send({mesage: 'Missin data'})
+    let product = new Product()
+    product.name = data.name
+    product.description = data.description
+    product.price = data.price
+
     product.save((err, productSaved)=>{
-        if(err)
+        if(err){
+            console.error(err)
+            return res.status(500).send({message: 'Error saving data'})
+        }else if(productSaved){
+            return res.send({mesage: 'Product saved successfully'})
+        }else{
+            return res.status(404).send({message: 'Error'})
+        }
     })
     return res.send({message: 'Ok', data})
 }
